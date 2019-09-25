41337c55b039ce8f88868ce1dfc05d8ef6f9daec
test <test>
2019-09-25 17:15:05 +0900

    TestCase#3

--- testeee.rb
+++ testeee.rb
@@ -1,13 +1,30 @@
-def assert_equal(a,b)
-  if a == b
-    puts 'OK'
-  else
-    puts 'NG'
+class TestCase
+  def assert_equal(a,b)
+    if a == b
+      puts 'OK'
+    else
+      puts 'NG'
+    end
   end
 end
 
 
+class SuccessTestCase < TestCase
+  def test
+    assert_equal(5, 2 + 3)
+  end
+end
+
+class FailureTestCase < TestCase
+  def test
+    assert_equal(6, 2 + 3)
+  end
+end
+
 
 if $0 == __FILE__
-  assert_equal(5,2+3);
+  success = SuccessTestCase.new
+  success.test # -> "OK"
+  failure = FailureTestCase.new
+  failure.test # -> "NG
 end
